3bdc23b34fe414e35f881933fedf87c6aee5ffe6
GerFox <gerfox@bk.ru>
2022-05-03 13:49:28 +0500

    Добавлен "Action menu"

--- app/src/main/java/com/example/gerfoxmessage/ui/fragments/Settings.kt
+++ app/src/main/java/com/example/gerfoxmessage/ui/fragments/Settings.kt
@@ -1,11 +1,17 @@
 package com.example.gerfoxmessage.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.gerfoxmessage.R
 
 class Settings : Base(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
