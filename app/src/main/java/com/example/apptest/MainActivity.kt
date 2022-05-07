c166fc9d68e3fb35e07b19370468eedba5aea03a
GerFox <gerfox@bk.ru>
2022-05-08 00:30:19 +0500

    Добавленна состояния пользователя "В сети", "Не в сети"

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -46,10 +46,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        AppStates.updateState(AppStates.ONLINE)
+    }
 
-
-
-
+    override fun onStop() {
+        super.onStop()
+        AppStates.updateState(AppStates.OFFLINE)
+    }
 
 }
 
